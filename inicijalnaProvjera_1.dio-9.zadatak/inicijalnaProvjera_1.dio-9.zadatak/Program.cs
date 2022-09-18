b6fccb2c52b09111d0cc44f2b14264a1c6964a97
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-09-19 00:24:43 +0200

    Add files via upload

---
+++ inicijalnaProvjera_1.dio-9.zadatak/inicijalnaProvjera_1.dio-9.zadatak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalnaProvjera_1.dio_9.zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>() { 4,7,45,3,11,25,1};
+            lista = lista.OrderBy(i => i).ToList();
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
